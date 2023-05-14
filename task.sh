de6d8a2d85fe4b914d31bd1805c6a5f4c11258d9
rabbit-cheng <cheng.donghui@gmail.com>
2023-05-14 21:08:13 +0800

    change docker-compose cmd

--- task.sh
+++ task.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker-compose run ora2pg 
-docker-compose run pgsql psql -h192.168.0.76 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill.sql
-docker-compose run pgsql psql -h192.168.0.76 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill_residential.sql
+# docker-compose run pgsql psql -h192.168.0.76 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill.sql
+# docker-compose run pgsql psql -h192.168.0.76 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill_residential.sql
+
+
+docker-compose run pgsql psql -h172.18.135.91 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill.sql
+docker-compose run pgsql psql -h172.18.135.91 -p25433 -UMuyuanWaterFactoryApp -w  -f /pgsql/import_equip_bill_residential.sql
